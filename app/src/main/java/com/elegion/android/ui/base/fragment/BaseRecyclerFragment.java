83847d11d3ee3f82a316ae4b67d3b872eff61c8a
mikhail.barannikov <mikhail.barannikov@e-legion.com>
2017-08-08 12:30:52 +0300

    Support annotations

--- app/src/main/java/com/elegion/android/ui/base/fragment/BaseRecyclerFragment.java
+++ app/src/main/java/com/elegion/android/ui/base/fragment/BaseRecyclerFragment.java
@@ -3,6 +3,7 @@ package com.elegion.android.ui.base.fragment;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
@@ -43,6 +44,7 @@ public abstract class BaseRecyclerFragment extends BaseRefresherFragment {
         return mRecyclerView.getAdapter().getItemCount() == 0;
     }
 
+    @LayoutRes
     protected int getLayout() {
         return R.layout.fr_recycler;
     }
