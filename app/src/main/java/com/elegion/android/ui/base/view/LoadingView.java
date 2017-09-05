9dfa05554ae840559d2d14f8121ccabbf988351e
mikhail.barannikov <mikhail.barannikov@e-legion.com>
2017-09-05 12:43:40 +0300

    Fixed a bug with loading when the app goes to background

--- app/src/main/java/com/elegion/android/ui/base/view/LoadingView.java
+++ app/src/main/java/com/elegion/android/ui/base/view/LoadingView.java
@@ -2,7 +2,7 @@ package com.elegion.android.ui.base.view;
 
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 import com.elegion.android.util.moxy.strategy.AddToEndSingleByTagStrategy;
-import com.elegion.android.util.moxy.strategy.SkipByTagStrategy;
+import com.elegion.android.util.moxy.strategy.OneExecutionByTagStrategy;
 
 /**
  * @author Mikhail Barannikov
@@ -11,7 +11,7 @@ public interface LoadingView {
     @StateStrategyType(value = AddToEndSingleByTagStrategy.class, tag = "LoadingView")
     void showLoadingIndicator();
 
-    @StateStrategyType(value = SkipByTagStrategy.class, tag = "LoadingView")
+    @StateStrategyType(value = OneExecutionByTagStrategy.class, tag = "LoadingView")
     void hideLoadingIndicator();
 
 }
