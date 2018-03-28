d467ba068ce1dd934e6cf2a07762fbf5326176a3
mikhail.barannikov <mikhail.barannikov@e-legion.com>
2018-03-28 10:57:41 +0300

    WIP: Converted some utils classes

---
+++ app/src/main/java/com/elegion/android/template/util/RxUtils.kt
@@ -0,0 +1,153 @@
+package com.elegion.android.template.util
+
+import android.support.v4.util.Pair
+import com.elegion.android.template.AppDelegate
+import com.elegion.android.template.data.Repository
+import com.elegion.android.template.data.model.ErrorBean
+import com.elegion.android.template.data.remote.rest.response.EmptyListResponse
+import com.elegion.android.template.ui.base.view.ErrorStubView
+import com.elegion.android.template.ui.base.view.ErrorView
+import com.elegion.android.template.ui.base.view.LoadingView
+import com.elegion.android.template.ui.base.view.NoInternetStubView
+import com.google.gson.JsonSyntaxException
+import io.reactivex.Flowable
+import io.reactivex.FlowableTransformer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import retrofit2.HttpException
+import timber.log.Timber
+import java.io.IOException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import java.util.*
+
+object RxUtils {
+    private val NETWORK_EXCEPTIONS = Arrays.asList<Class<*>>(
+            UnknownHostException::class.java,
+            SocketTimeoutException::class.java,
+            ConnectException::class.java
+    )
+
+    @JvmStatic
+    fun dispose(subscription: CompositeDisposable?) {
+        if (subscription != null && !subscription.isDisposed) {
+            subscription.dispose()
+        }
+    }
+
+    @JvmStatic
+    fun isNullOrUnsubscribed(disposable: Disposable?): Boolean {
+        return disposable == null || disposable.isDisposed
+    }
+
+    @JvmStatic
+    fun <T> async(flowable: Flowable<T>): Flowable<T> {
+        return flowable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    @JvmStatic
+    fun <T> loading(view: LoadingView): FlowableTransformer<T, T> {
+        return FlowableTransformer {
+            it.doOnSubscribe {
+                view.showLoadingIndicator()
+                Timber.d("LOADING: doOnSubscribe")
+            }.doFinally {
+                Timber.d("LOADING: doOnTerminate")
+                view.hideLoadingIndicator()
+            }
+        }
+    }
+
+    @JvmStatic
+    fun <T : EmptyListResponse> emptyErrorStub(view: ErrorStubView): FlowableTransformer<T, T> {
+        return FlowableTransformer {
+            it.doOnSubscribe {
+                view.hideErrorStub(it)
+            }.doOnNext {
+                if (it.isEmpty) {
+                    view.showErrorStub()
+                }
+            }
+        }
+    }
+
+    @JvmStatic
+    fun <F : EmptyListResponse, S : EmptyListResponse, T : Pair<F, S>> emptyPairErrorStub(view: ErrorStubView): FlowableTransformer<T, T> {
+        return FlowableTransformer {
+            it.doOnSubscribe {
+                view.hideErrorStub(it)
+            }.doOnNext {
+                if (it.first != null && it.first!!.isEmpty && it.second != null && it.second!!.isEmpty) {
+                    view.showErrorStub()
+                }
+            }
+        }
+    }
+
+    @JvmStatic
+    fun <T> errorTransformer(errorView: ErrorView,
+                             repository: Repository,
+                             noInternetStubView: NoInternetStubView?): FlowableTransformer<T, T> {
+        return FlowableTransformer {
+            it.doOnNext {
+                noInternetStubView?.hideNoInternetStub()
+            }.doOnError(error(errorView, repository))
+        }
+    }
+
+    @JvmStatic
+    fun error(errorView: ErrorView, repository: Repository): Consumer<Throwable> {
+        return Consumer {
+            Timber.d(it, "from RxUtils.error")
+            handleError(it, errorView, repository)
+        }
+    }
+
+    @JvmStatic
+    private fun handleError(e: Throwable, errorView: ErrorView, repository: Repository) {
+        if (e is HttpException) {
+            if (e.code() == 401) {
+                errorView.showErrorMessage(e.message())
+                AuthUtils.logout(repository)
+                AuthUtils.openLogin(AppDelegate.appContext)
+            } else {
+                try {
+                    val errorBody = e.response().errorBody()!!.string()
+                    val errorBean = GsonUtils.requestGson().fromJson(errorBody, ErrorBean::class.java)
+                    if (errorBean != null) {
+                        errorView.showErrorMessage(errorBean.message)
+                    } else {
+                        errorView.showErrorMessage(e.message())
+                    }
+                } catch (e1: IOException) {
+                    errorView.showErrorMessage(e.message())
+                } catch (e1: IllegalStateException) {
+                    errorView.showErrorMessage(e.message())
+                } catch (e1: JsonSyntaxException) {
+                    errorView.showErrorMessage(e.message())
+                }
+
+            }
+        } else if (NETWORK_EXCEPTIONS.contains(e.javaClass)) {
+            errorView.showNetworkError()
+        } else {
+            errorView.showUnexpectedError()
+        }
+    }
+
+    @JvmStatic
+    fun errorLogE(e: Throwable) {
+        Timber.d(e, "errorLogE")
+    }
+
+    @JvmStatic
+    fun <T> emptyTransformer(): FlowableTransformer<T, T> {
+        return FlowableTransformer { it }
+    }
+}
---
+++ app/src/main/java/com/elegion/android/template/util/ViewUtils.kt
@@ -0,0 +1,221 @@
+package com.elegion.android.template.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.support.annotation.ColorInt
+import android.support.annotation.FloatRange
+import android.support.annotation.IdRes
+import android.support.annotation.IntDef
+import android.support.v4.content.ContextCompat
+import android.support.v7.content.res.AppCompatResources
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.inputmethod.InputMethodManager
+import android.widget.CompoundButton
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
+
+object ViewUtils {
+    @JvmStatic
+    private val DP_DENSITY = 160f
+    @JvmStatic
+    private val MAX_OPACITY = 255f
+
+    @JvmStatic
+    val displayWidth: Int
+        get() = Resources.getSystem().displayMetrics.widthPixels
+
+    @JvmStatic
+    val displayHeight: Int
+        get() = Resources.getSystem().displayMetrics.heightPixels
+
+    @IntDef(View.VISIBLE, View.INVISIBLE, View.GONE)
+    @Retention(AnnotationRetention.SOURCE)
+    annotation class Visibility
+
+    @JvmStatic
+    fun pxToDp(px: Float): Float {
+        val densityDpi = Resources.getSystem().displayMetrics.densityDpi.toFloat()
+        return px / (densityDpi / DP_DENSITY)
+    }
+
+    @JvmStatic
+    fun dpToPx(dp: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().displayMetrics).toInt()
+    }
+
+    @JvmStatic
+    fun spToPx(sp: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, Resources.getSystem().displayMetrics).toInt()
+    }
+
+    @JvmStatic
+    fun hideKeyboard(activity: Activity?) {
+        if (activity != null) {
+            val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            val window = activity.window
+            if (imm != null && window != null) {
+                var currentFocus = window.currentFocus
+                if (currentFocus == null) {
+                    currentFocus = window.decorView.findFocus()
+                }
+                if (currentFocus != null) {
+                    imm.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+                    currentFocus.clearFocus()
+                }
+            }
+        }
+    }
+
+    @JvmStatic
+    fun hideKeyboard(context: Context?, view: View) {
+        if (context != null) {
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    @JvmStatic
+    fun setVisibility(@Visibility visibility: Int, vararg views: View) {
+        if (views == null || views.size == 0) {
+            return
+        }
+        for (v in views) {
+            if (v != null && v.visibility != visibility) {
+                v.visibility = visibility
+            }
+        }
+    }
+
+    @JvmStatic
+    fun makeVisibleOrGone(visible: Boolean, vararg views: View) {
+        if (views == null || views.size == 0) {
+            return
+        }
+        setVisibility(if (visible) View.VISIBLE else View.GONE, *views)
+    }
+
+    @JvmStatic
+    fun makeVisibleOrInvisible(visible: Boolean, vararg views: View) {
+        if (views == null || views.size == 0) {
+            return
+        }
+        setVisibility(if (visible) View.VISIBLE else View.INVISIBLE, *views)
+    }
+
+    @JvmStatic
+    fun removeOnGlobalLayoutListener(v: View, listener: ViewTreeObserver.OnGlobalLayoutListener) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            v.viewTreeObserver.removeGlobalOnLayoutListener(listener)
+        } else {
+            v.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
+    @JvmStatic
+    fun changeEditTextBottomLineColor(editText: EditText, @ColorInt color: Int) {
+        editText.background.mutate().setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+    }
+
+    @JvmStatic
+    fun setEditTextCursorColor(view: EditText, @ColorInt color: Int) {
+        try {
+            // Get the cursor resource id
+            var field = TextView::class.java.getDeclaredField("mCursorDrawableRes")
+            field.isAccessible = true
+            val drawableResId = field.getInt(view)
+
+            // Get the editor
+            field = TextView::class.java.getDeclaredField("mEditor")
+            field.isAccessible = true
+            val editor = field.get(view)
+
+            // Get the drawable and set a color filter
+            val drawable = ContextCompat.getDrawable(view.context, drawableResId)
+            drawable!!.setColorFilter(color, PorterDuff.Mode.SRC_IN)
+            val drawables = arrayOf(drawable, drawable)
+
+            // Set the drawables
+            field = editor.javaClass.getDeclaredField("mCursorDrawable")
+            field.isAccessible = true
+            field.set(editor, drawables)
+        } catch (ignored: Exception) {
+            // ignored
+        }
+    }
+
+    @JvmStatic
+    fun getColorWithOpacity(@FloatRange(from = 0.0, to = 1.0) opacity: Float, @ColorInt color: Int): Int {
+        return Color.argb((MAX_OPACITY * opacity).toInt(), Color.red(color), Color.green(color), Color.blue(color))
+    }
+
+    @JvmStatic
+    fun setCheckedWithoutNotify(button: CompoundButton, isChecked: Boolean,
+                                listener: CompoundButton.OnCheckedChangeListener) {
+        button.setOnCheckedChangeListener(null)
+        button.isChecked = isChecked
+        button.setOnCheckedChangeListener(listener)
+    }
+
+    @JvmStatic
+    fun getEditTextString(editText: EditText): String {
+        val editable = editText.text
+        return editable?.toString() ?: ""
+    }
+
+    /**
+     * REGION PROJECT SPECIFIC TOOLS
+     */
+
+    @JvmStatic
+    fun <T> findView(activity: Activity, @IdRes id: Int): T {
+        return activity.findViewById<View>(id) as T
+    }
+
+    @JvmStatic
+    fun <T> findView(view: View, @IdRes id: Int): T {
+        return view.findViewById<View>(id) as T
+    }
+
+    @JvmStatic
+    fun <T : View> findViews(activity: Activity, @IdRes vararg ids: Int): List<T> {
+        val result = ArrayList<T>(ids.size)
+        for (id in ids) {
+            result.add(ViewUtils.findView(activity, id))
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun <T : View> findViews(view: View, @IdRes vararg ids: Int): List<T> {
+        val result = ArrayList<T>(ids.size)
+        for (id in ids) {
+            result.add(ViewUtils.findView(view, id))
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun getTypedArrayDrawable(context: Context, attributeArray: TypedArray, styleIndex: Int): Drawable? {
+        val result: Drawable?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            result = attributeArray.getDrawable(styleIndex)
+        } else {
+            val resId = attributeArray.getResourceId(styleIndex, -1)
+            result = if (resId == -1) null else AppCompatResources.getDrawable(context, resId)
+        }
+        return result
+    }
+
+    /**
+     * ENDREGION PROJECT SPECIFIC TOOLS
+     */
+}
