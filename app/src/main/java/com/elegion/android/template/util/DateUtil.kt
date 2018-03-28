1912d5b1f3132cc7819d3b1ec30cd75c9aac6967
mikhail.barannikov <mikhail.barannikov@e-legion.com>
2018-03-28 11:41:24 +0300

    WIP: Converted all util classes

---
+++ app/src/main/java/com/elegion/android/template/util/DateUtil.kt
@@ -0,0 +1,42 @@
+package com.elegion.android.template.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtil {
+
+    private const val DD_MM_YYYY = "dd.MM.yyyy"
+    private const val SERVER_ISO8601_DATE = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    private const val TIMEZONE_UTC = "UTC"
+
+    @JvmStatic
+    val calendar: Calendar
+        get() = Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE_UTC))
+
+    @JvmStatic
+    val todayDate: Date
+        get() = Date(Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE_UTC)).timeInMillis)
+
+    @JvmStatic
+    fun getDateString(date: Date?): String {
+        if (date == null) {
+            return ""
+        }
+
+        return SimpleDateFormat(DD_MM_YYYY, Locale.getDefault()).format(date)
+    }
+
+    @JvmStatic
+    fun getCalendar(date: Date): Calendar {
+        return Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE_UTC)).apply {
+            time = date
+        }
+    }
+
+    @JvmStatic
+    fun getCalendar(millis: Long): Calendar {
+        return Calendar.getInstance(TimeZone.getTimeZone(TIMEZONE_UTC)).apply {
+          timeInMillis = millis
+        }
+    }
+}
