2470703079860da7a50473e10804c0fe5f7f43dd
mikhail.barannikov <mikhail.barannikov@e-legion.com>
2018-03-30 10:41:09 +0300

    TextWatcher power

--- app/src/main/java/com/elegion/android/template/ui/login/LoginFragment.kt
+++ app/src/main/java/com/elegion/android/template/ui/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.elegion.android.template.ui.login
 
 import android.support.v4.app.Fragment
+import android.text.TextWatcher
 import android.view.View
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
@@ -8,24 +9,15 @@ import com.elegion.android.template.R
 import com.elegion.android.template.data.Repository
 import com.elegion.android.template.ui.base.fragment.BaseNoInternetFragment
 import com.elegion.android.template.ui.features.FeaturesActivity
-import com.elegion.android.template.util.SimpleTextWatcher
+import com.elegion.android.template.util.addTextChangedListener
 import kotlinx.android.synthetic.main.fr_login.*
 
 class LoginFragment : BaseNoInternetFragment(), LoginView {
     @InjectPresenter
     internal lateinit var presenter: LoginPresenter
 
-    private val loginEmailTexTWatcher = object : SimpleTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
-            presenter.setEmail(text.toString())
-        }
-    }
-
-    val loginPasswordTexTWatcher = object : SimpleTextWatcher() {
-        override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
-            presenter.setPassword(text.toString())
-        }
-    }
+    private lateinit var loginEmailTexTWatcher: TextWatcher
+    private lateinit var loginPasswordTexTWatcher: TextWatcher
 
     @ProvidePresenter
     internal fun providePresenter(): LoginPresenter = LoginPresenter(Repository.get(activity!!))
@@ -36,8 +28,12 @@ class LoginFragment : BaseNoInternetFragment(), LoginView {
         super.onResume()
         loginBtn.setOnClickListener { presenter.login() }
         loginLetMeInBtn.setOnClickListener { presenter.letMeIn() }
-        loginEmail.addTextChangedListener(loginEmailTexTWatcher)
-        loginPassword.addTextChangedListener(loginPasswordTexTWatcher)
+        loginEmailTexTWatcher = loginEmail.addTextChangedListener {
+            onTextChanged { text, _, _, _ -> presenter.setEmail(text.toString()) }
+        }
+        loginPasswordTexTWatcher = loginPassword.addTextChangedListener {
+            onTextChanged { text, _, _, _ -> presenter.setPassword(text.toString()) }
+        }
     }
 
     override fun onPause() {
