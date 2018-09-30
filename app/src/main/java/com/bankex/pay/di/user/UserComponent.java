7f3bf567f860efd85aacf09d4833304593ea2efc
Pavel Apanovskiy <apanovskiyp@gmail.com>
2018-09-30 17:26:46 +0300

    WALLET-447 Создать экран Receive с данными-заглушкой

--- app/src/main/java/com/bankex/pay/di/user/UserComponent.java
+++ app/src/main/java/com/bankex/pay/di/user/UserComponent.java
@@ -1,9 +1,10 @@
 package com.bankex.pay.di.user;
 
 import com.bankex.pay.di.importorcreate.ImportOrCreateComponent;
-import com.bankex.pay.di.mainscreen.MainScreenComponent;
-import com.bankex.pay.di.setpin.SetPinComponent;
 import com.bankex.pay.di.lockscreen.LockScreenComponent;
+import com.bankex.pay.di.mainscreen.MainScreenComponent;
+import com.bankex.pay.di.receive.ReceiveComponent;
+import com.bankex.pay.di.setpin.SetPinComponent;
 
 import dagger.Subcomponent;
 
@@ -27,6 +28,12 @@ public interface UserComponent {
     MainScreenComponent.Builder plusMainScreenComponentBuilder();
 
     ImportOrCreateComponent.Builder plusImportOrCreateComponentBuilder();
+
     SetPinComponent.Builder plusSetPinComponentBuilder();
+
     LockScreenComponent.Builder plusLockScreenComponentBuilder();
+
+    // TODO: 30/09/2018 возможно - унести в компонент инфо о токене
+    ReceiveComponent.Builder plusReceiveComponent();
+
 }
