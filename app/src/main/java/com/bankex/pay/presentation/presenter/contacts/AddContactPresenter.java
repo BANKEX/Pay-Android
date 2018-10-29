ae387b5465973d3ed2df18fa4f1de7933d9f43f1
Natalia Melkonyan <melkonyan@MacBook-Pro-Natalia.local>
2018-10-29 13:24:43 +0300

    WIP.

---
+++ app/src/main/java/com/bankex/pay/presentation/presenter/contacts/AddContactPresenter.java
@@ -0,0 +1,10 @@
+package com.bankex.pay.presentation.presenter.contacts;
+
+import com.arellomobile.mvp.InjectViewState;
+
+/**
+ * Presenter for screen to add new contact.
+ */
+@InjectViewState
+public class AddContactPresenter {
+}
