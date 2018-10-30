1309e2dba57cce02129a3aca08062527f10f3a8f
Natalia Melkonyan <melkonyan@MacBook-Pro-Natalia.local>
2018-10-30 18:20:43 +0300

    Creating contact info screen.

---
+++ app/src/main/java/com/bankex/pay/presentation/ui/contactinfo/ContactInfoFragment.java
@@ -0,0 +1,4 @@
+package com.bankex.pay.presentation.ui.contactinfo;
+
+public class ContactInfoFragment {
+}
---
+++ app/src/main/java/com/bankex/pay/presentation/ui/contactinfo/IContactInfoView.java
@@ -0,0 +1,4 @@
+package com.bankex.pay.presentation.ui.contactinfo;
+
+public interface IContactInfoView {
+}
